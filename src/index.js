7739ad46c0fa0a6e150e6e88626c776364a781ff
Todd Chavez <84374128+ifbwima@users.noreply.github.com>
2023-11-08 16:43:28 +0100

    questionnaire 1 à 10

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/'
 import Survey from './pages/Survey/'
 import Header from './components/Header'
+import Error from './components/Error'
+import Freelances from './pages/Freelances'
+import Results from './pages/Results'
 
-ReactDOM.render(
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/survey" element={<Survey />} />
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
+
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
